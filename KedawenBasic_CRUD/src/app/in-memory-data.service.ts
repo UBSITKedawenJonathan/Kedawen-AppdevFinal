160337dbb7c055c233b1be3eda1162740f1c4c47
UBSITKedawenJonathan <97932183+UBSITKedawenJonathan@users.noreply.github.com>
2022-05-19 20:42:07 +0800

    hello

--- KedawenBasic_CRUD/src/app/in-memory-data.service.ts
+++ KedawenBasic_CRUD/src/app/in-memory-data.service.ts
@@ -8,11 +8,11 @@ import { Item } from './item';
 export class InMemoryDataService implements InMemoryDataService{
   createDb() {
     const items = [
-      {  id: 1,  pname: 'Buko',  type: 'refreshment',  stock: 5,  pprice: 50.00,  fname: 'Jonathan',  lname: 'kedawen',  email: 'kedawenjr123@yahoo.sdaasd',  mobnum: '09999999999',  dob: new Date(2012, 1, 1),pic:"assets/buko.png"
+      {  id: 1,  pname: 'Buko',  type: 'refreshment',  stock: 5,  pprice: 50.00,  fname: 'Jonathan',  lname: 'kedawen',  email: 'kedawenjr123@yahoo.sdaasd',  mobnum: '09999999999',  dob: new Date(2012, 1, 1),pic:"../assets/buko.png"
    },
-   {id: 2, pname: 'Ice', type: 'malamig', stock: 122222, pprice: 269.00, fname: 'Jonathan', lname: 'kedawen', email: 'kedawenjr123@yahoo.sdaasd',  mobnum: '09999999999',  dob: new Date(2012, 1, 1),pic:"assets/ice.png"
+   {id: 2, pname: 'Ice', type: 'malamig', stock: 122222, pprice: 269.00, fname: 'Jonathan', lname: 'kedawen', email: 'kedawenjr123@yahoo.sdaasd',  mobnum: '09999999999',  dob: new Date(2012, 1, 1),pic:"../assets/ice.png"
     },
-    { id: 3, pname: 'Cokes', type: 'sopdrink', stock: 3, pprice: 899.00, fname: 'Jonathan', lname: 'kedawen', email: 'kedawenjr123@yahoo.sdaasd',  mobnum: '09999999999',  dob: new Date(2012, 1, 1),pic:"assets/milk.png"
+    { id: 3, pname: 'Cokes', type: 'sopdrink', stock: 3, pprice: 899.00, fname: 'Jonathan', lname: 'kedawen', email: 'kedawenjr123@yahoo.sdaasd',  mobnum: '09999999999',  dob: new Date(2012, 1, 1),pic:"../assets/milk.png"
     }
     ];
     return {items}
